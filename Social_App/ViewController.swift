83f037d2fcca38da90867e04a69fe477285a8e54
Gabriel Lewis <benbowgl1@gmail.com>
2016-01-23 15:13:51 -0500

    Initial Commit

---
+++ Social_App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Social_App
+//
+//  Created by Gabriel Benbow on 1/23/16.
+//  Copyright © 2016 Gabriel Benbow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
