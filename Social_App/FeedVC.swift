5a9f8aac90a74a13717f923ab55c89cc82b06edd
Gabriel Lewis <benbowgl1@gmail.com>
2016-02-15 13:47:38 -0500

    Feed only shows following users's posts

--- Social_App/FeedVC.swift
+++ Social_App/FeedVC.swift
@@ -17,35 +17,57 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 	@IBOutlet weak var feedTV: UITableView!
 	
 	var posts = [Post]()
+	var postKeys = [String]()
 	var post: Post!
 	var user: User!
+	var following = [String]()
 	static var imageCache = NSCache()
 	var cell: PostCell!
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-
+		
+	
 
 		feedTV.delegate = self
 		feedTV.dataSource = self
-		
-		DataService.ds.REF_POSTS.observeEventType(.Value, withBlock: { snapShot in
+		DataService.ds.REF_USER_CURRENT.childByAppendingPath("following").observeEventType(.Value, withBlock: { snapshot in
 			
-			if let snapshots = snapShot.children.allObjects as? [FDataSnapshot] {
-				self.posts = []
-				for snap in snapshots {
-					
-					
-					if let postDict = snap.value as? Dictionary<String,AnyObject> {
-						let key = snap.key
-						let post = Post(postKey: key, dictionary: postDict)
-						self.posts.append(post)
-					}
-				}
+			for snap in snapshot.value as! Dictionary<String, AnyObject> {
+				
+				self.following.append(snap.0)
+				//print(self.following)
 			}
-			self.sortList()
+			
+			self.getFollowersPostKeys()
+			
 		})
+		
+		
+		
+		
+//		DataService.ds.REF_POSTS.observeEventType(.Value, withBlock: { snapShot in
+//			
+//			if let snapshots = snapShot.children.allObjects as? [FDataSnapshot] {
+//				self.posts = []
+//				for snap in snapshots {
+//					
+//					if let postDict = snap.value as? Dictionary<String,AnyObject> {
+//						let key = snap.key
+//						let post = Post(postKey: key, dictionary: postDict)
+//						self.posts.append(post)
+//					}
+//				}
+//			}
+//			self.sortList()
+//		})
 	}
+	override func viewWillAppear(animated: Bool) {
+		super.viewWillAppear(true)
+		
+
+	}
+
 	
 	func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 		return posts.count
@@ -93,11 +115,72 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 	
 	func sortList() {
 		posts.sortInPlace() { $0.date > $1.date }
+		
 		self.feedTV.reloadData()
 	}
 	
+	func someThing() {
 	
+		
+		
 
+	}
+	
+	
+	
+	func getFollowersPostKeys() {
+		var x = 0
+		
+		while x != following.count {
+			
+			let userkey = following[x]
+			
+			DataService.ds.REF_USERS.childByAppendingPath(userkey).childByAppendingPath("posts").observeEventType(.Value, withBlock: { snapshot in
+				for snap in snapshot.value as! Dictionary<String, AnyObject> {
+					self.postKeys.append(snap.0)
+					
+				}
+				self.getPosts()
+			})
+			x++
+			if x == self.following.count {
+				
+				
+			}
+			
+			
+		}
+		
+	
+		
+	}
+	
+	func getPosts() {
+		
+		var x = 0
+		
+		while x != postKeys.count {
+			self.posts = []
+
+			let postkey = postKeys[x]
+			
+			DataService.ds.REF_POSTS.childByAppendingPath(postkey).observeEventType(.Value, withBlock: { snapshot in
+				
+				let postDict = snapshot.value as! Dictionary<String, AnyObject>
+				let post = Post(postKey: snapshot.key, dictionary: postDict)
+				self.posts.append(post)
+				self.sortList()
+			
+			})
+			
+			x++
+			if x == self.postKeys.count {
+				print(x)
+							}
+			
+		}
+		
+	}
 	
 	
 	
